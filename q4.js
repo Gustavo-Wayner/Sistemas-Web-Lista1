c1498f3f0a90751e1ebe1368d40880070aef4ce7
Gustavo-Wayner <gusgabway@gmail.com>
2026-08-25 10:40:06 -0300

    Trocando o objeto retornado pelo Resolve por uma string

--- q4.js
+++ q4.js
@@ -3,18 +3,13 @@ function consultarEstoque(produto) {
     setTimeout(() => {
       let quant = Math.floor(Math.random() * 20);
 
-      if (quant > 0)
-      {
-        resolve({
-          Produto: produto,
-          Quantidade: quant
-        })
+      if (quant > 0) {
+        resolve(`Produto: ${produto};\nQuantidade: ${quant};`);
       }
-      else
-      {
+      else {
         reject(`Produto ${produto} está esgotado!`);
       }
-    }, 2000)
+    }, 2000);
   })
 }
 
